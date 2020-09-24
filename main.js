0a9eb66104c1c64c9b092e30d9f1b4ac5f81ee6f
Aaron Diggdon <aarondiggdon@gmail.com>
2020-09-23 23:42:59 -0700

    Added questions and answers that respond to buttons

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(function() {
   var startgame = document.querySelector(".btn");
-  var question = document.querySelector(".cover-heading");
+  var questionText = document.querySelector(".cover-heading");
   var hintText = document.querySelector(".lead")
   var timeEl = document.querySelector(".masthead-brand");
-  var score = document.querySelector(".nav");
+  var score = document.querySelector(".score");
 
   var one = document.getElementById("one");    
   var two = document.getElementById("two"); 
@@ -14,55 +14,60 @@ $(function() {
   allbuttons.style.display = "none";
 
 
-  score = 0;
+  scorenum = 0;
 
 
-  i = 0;
+  
 
   var questions = [{
     question: "When was the first photographic snapshot taken?",
-    choices: {
       a: "1776",
       b: "1914",
       c: "1826",
-      d: "2020"},
-    textanswer: ["i need sleep"],
+      d: "2020",
+    answer: "1826"
     },
     {
-      question: ["help"],
-      choices: {
-        a: "goodbye",
-        b: "my",
-        c: "name",
-        d: "is"},
-      textanswer: { c: "help"}
+      question: "Who took the first color photograph?",
+        a: "Henry Ford",
+        b: "James Maxwell",
+        c: "Thomas Jefferson",
+        d: "Tom Cruise",
+      answer: "James Maxwell"
     },
     {
-      question: ["How many"],
-      choices: {
-        a: "hello",
-        b: "my",
-        c: "name",
-        d: "is"},
-      textanswer: ["i need sleep"],
+      question: "When was the term 'megapixel' first used?",
+        a: "1929",
+        b: "1889",
+        c: "1946",
+        d: "1984",
+      answer: "1984"
       },
       {
-        question: ["dear god plz let me sleep"],
-        choices: {
-          a: "goodbye",
-          b: "my",
-          c: "name",
-          d: "is"},
-        textanswer: ["i need sleep"],
+        question: "When was the first color photograph taken?",
+          a: "1861",
+          b: "1826",
+          c: "1912",
+          d: "1946",
+        answer: "1861"
       },
+      {
+        question: "When was the term 'megapixel' first used?",
+          a: "1929",
+          b: "1889",
+          c: "1946",
+          d: "1984",
+        answer: "1984"
+        },
     ];
-
-    var answerkey = [three, two, three, four];
 
 
   var secondsLeft = 100;
         
   
+
+  i = 0;
+
   startgame.addEventListener("click", function(){
 
     //Button Functions
@@ -70,104 +75,94 @@ $(function() {
     startgame.remove();
     allbuttons.style.display = "contents";
 
-
-      //QUESTIONS
-
-      // var choice = document.querySelectorAll("#one, #two, #three, #four");
-      // console.log(choice);
-      // choice[0].textContent = "1";
-      // choice[1].textContent = "world";
-      // choice[2].textContent = ":)"
-      // choice[3].textContent = ":)"
-
-      // var points = 0;
-      
    setTime();
    ask();
 
 
   });
-0
+
   function ask(){
     
-      question.textContent = questions[i]["question"];
-      var answer = ans[i];
-      one.textContent = questions[i]["choices"]["a"];
-      two.textContent = questions[i]["choices"]["b"];
-      three.textContent = questions[i]["choices"]["c"];
-      four.textContent = questions[i]["choices"]["d"];
-
+      questionText.textContent = questions[i].question;
+      one.textContent = questions[i].a;
+      two.textContent = questions[i].b;
+      three.textContent = questions[i].c;
+      four.textContent = questions[i].d;
       
-      console.log(answer);
-
       
-      one.addEventListener("click", function(event) {
-     
-        if (event.target.matches(answer)) {
-          
-          score.textContent(score++);
-          alert("Correct!");
-          i++;
-          ask();
-        } else {
-          alert("Incorrect, the correct answer was " + textanswer);
-          i++;
-          ask();
+      one.addEventListener("click", function() {
+        
+        if (one.textContent === questions[i].answer) {
+          console.log(questions[i].answer);
+          scorenum++;
+          // alert("Correct!");
+        } 
+        
+        else {
+          // alert("Incorrect, the correct answer was " + questions[i].answer);
+          console.log(questions[i].answer);
         }
+        i++;
+        ask();
+        return;
       });
 
       
-      two.addEventListener("click", function(event) {
-     
-        if (event.target.matches(answer)) {
+      two.addEventListener("click", function() {
+        
+        if (two.textContent === questions[i].answer) {
+          console.log(questions[i].answer);
+          scorenum++;
           
-          score.textContent(score++);
-          alert("Correct!");
-          i++;
-          ask();
-        } else {
-          alert("Incorrect, the correct answer was " + textanswer);
-          i++;
-          ask();
+        } 
+        
+        else {
+          console.log(questions[i].answer);
         }
+        i++;
+        ask();
+        return;
       });
       
-      three.addEventListener("click", function(event) {
-     
-        if (event.target.matches(answer)) {
-          
-          score.textContent(score++);
-          alert("Correct!");
-          i++;
-          ask();
-        } else {
-          alert("Incorrect, the correct answer was " + textanswer);
-          i++;
-          ask();
+      three.addEventListener("click", function() {
+        if (three.textContent === questions[i].answer) {
+          console.log(questions[i].answer);
+          scorenum++;
+         
+        } 
+        
+        else {
+          console.log(questions[i].answer);
         }
+        i++;
+        ask();
+        return;
       });
       
-      four.addEventListener("click", function(event) {
+      four.addEventListener("click", function() {
      
-        if (event.target.matches(answer)) {
+        if (four.textContent === questions[i].answer) {
+          console.log(questions[i].answer);
+          scorenum++;
           
-          score.textContent(score++);
-          alert("Correct!");
-          i++;
-          ask();
-        } else {
-          alert("Incorrect, the correct answer was " + textanswer);
-          i++;
-          ask();
+        } 
+        
+        else {
+          console.log(questions[i].answer);
         }
+        i++;
+        ask();
+        return;
       });
       // if (i == questions.length){
       // counter == 0
 
       // }
       
-      
+      score.textContent = scorenum;
+
     }
+    
 
 
     function setTime() {
